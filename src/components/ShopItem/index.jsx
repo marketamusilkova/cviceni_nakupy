92a8a801a4808c45e91c76057f65e9cddd2adf5a
Markéta Musílková <mrs.musilkova@gmail.com>
2024-05-03 20:46:29 +0200

    Přidání možnosti mazat položky a označovat nakoupené

--- src/components/ShopItem/index.jsx
+++ src/components/ShopItem/index.jsx
@@ -1,13 +1,18 @@
 import './style.css';
 
-export const ShopItem = ({ name, amount, bought }) => {
+export const ShopItem = ({ name, amount, bought, id }) => {
   const tickClass = bought ? 'btn-tick btn-tick--on' : 'btn-tick';
   return (
     <div className="shopitem">
       <button className={tickClass} />
       <div className="shopitem__name">{name}</div>
       <div className="shopitem__amount">{amount}</div>
-      <button className="btn-delete">Smazat</button>
+      <button className="btn-delete" data-id={id}>
+        Smazat
+      </button>
+      <button className="btn-bought" data-id={id}>
+        Nakoupeno
+      </button>
     </div>
   );
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,48 +4,49 @@ import '../global.css';
 import './index.css';
 
 // TODO nezapomeňte nastavit svůj login – jednoznačný identifikátor (třeba název účtu na GitHubu)
-//const login = ""
+const login = 'Marketa_M.';
 
-const response = await fetch(
-  'https://nakupy.czechitas.dev/api/mon',
-  {
-    headers: {
-      Authorization: login,
-    },
+const response = await fetch('https://nakupy.czechitas.dev/api/mon', {
+  headers: {
+    Authorization: login,
   },
-);
+});
 const list = await response.json();
 
-const HomePage = () => (
-  <>
-    <div className="banner"></div>
-    <div className="container">
-      <form className="newitem-form">
-        <label htmlFor="input-name">Položka</label>
-        <input id="input-name" type="text" />
-        <label htmlFor="input-amount">Množství</label>
-        <input id="input-amount" type="text" />
-        <label htmlFor="input-unit">Jednotka</label>
-        <input id="input-unit" type="text" />
-        <button className="btn-add">Přidat</button>
-      </form>
-      <div className="shoplist">
-        {list.map((item) => (
-          <ShopItem 
-            key={item.id}
-            name={item.product}
-            amount={item.amount + ' ' + item.unit}
-            bought={item.done}
-          />
-        ))}
+const HomePage = () => {
+  return (
+    <>
+      <div className="banner"></div>
+      <div className="container">
+        <form className="newitem-form">
+          <label htmlFor="input-name">Položka</label>
+          <input id="input-name" type="text" />
+          <label htmlFor="input-amount">Množství</label>
+          <input id="input-amount" type="text" />
+          <label htmlFor="input-unit">Jednotka</label>
+          <input id="input-unit" type="text" />
+          <button className="btn-add">Přidat</button>
+        </form>
+        <div className="shoplist">
+          {list.map((item) => (
+            <ShopItem
+              key={item.id}
+              name={item.product}
+              amount={item.amount + ' ' + item.unit}
+              bought={item.done}
+              id={item.id}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 document.querySelector('#root').innerHTML = render(<HomePage />);
 
-document.querySelector('.newitem-form')
+document
+  .querySelector('.newitem-form')
   .addEventListener('submit', async (e) => {
     e.preventDefault();
 
@@ -60,18 +61,48 @@ document.querySelector('.newitem-form')
       done: false,
     };
 
-    await fetch(
-      'https://nakupy.czechitas.dev/api/mon',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: login,
-        },
-        body: JSON.stringify(body),
+    await fetch('https://nakupy.czechitas.dev/api/mon', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: login,
       },
-    );
+      body: JSON.stringify(body),
+    });
 
     window.location.reload();
-  }
-);
+  });
+
+const deleteButtons = document.querySelectorAll('.btn-delete');
+deleteButtons.forEach((btn) => {
+  btn.addEventListener('click', async (e) => {
+    const btnId = e.target.dataset.id;
+    console.log(btnId)
+    await fetch(`https://nakupy.czechitas.dev/api/mon/${btnId}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: login,
+      },
+    });
+    window.location.reload();
+  });
+});
+
+const boughtButtons = document.querySelectorAll('.btn-bought');
+boughtButtons.forEach((btn) => {
+  btn.addEventListener("click", async (e) => {
+    const btnId = e.target.dataset.id;
+    console.log(btnId)
+    await fetch(`https://nakupy.czechitas.dev/api/mon/${btnId}`, {
+      method: 'PATCH',
+      headers: {
+        Authorization: login,
+      },
+      body: JSON.stringify({
+        "done": true
+      })
+    });
+    window.location.reload();
+  })
+})
+
